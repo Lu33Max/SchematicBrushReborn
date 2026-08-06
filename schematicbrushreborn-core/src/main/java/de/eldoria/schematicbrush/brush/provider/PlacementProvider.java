7cc494f61bf449efe6a361e3cb25655c5f223c80
Lu33Max <ernst_lucas@outlook.de>
2026-08-06 12:58:53 +0200

    change default placement to original

--- schematicbrushreborn-core/src/main/java/de/eldoria/schematicbrush/brush/provider/PlacementProvider.java
+++ schematicbrushreborn-core/src/main/java/de/eldoria/schematicbrush/brush/provider/PlacementProvider.java
@@ -6,6 +6,12 @@
 
 package de.eldoria.schematicbrush.brush.provider;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.entity.Player;
+
 import de.eldoria.eldoutilities.commands.command.util.Arguments;
 import de.eldoria.schematicbrush.brush.config.placement.APlacement;
 import de.eldoria.schematicbrush.brush.config.placement.Bottom;
@@ -16,11 +22,6 @@ import de.eldoria.schematicbrush.brush.config.placement.Raise;
 import de.eldoria.schematicbrush.brush.config.placement.Top;
 import de.eldoria.schematicbrush.brush.config.provider.ModifierProvider;
 import de.eldoria.schematicbrush.brush.config.provider.Mutator;
-import org.bukkit.configuration.serialization.ConfigurationSerializable;
-import org.bukkit.entity.Player;
-
-import java.util.Collections;
-import java.util.List;
 
 public abstract class PlacementProvider extends ModifierProvider {
     private static final APlacement bottom = new Bottom();
@@ -56,7 +57,7 @@ public abstract class PlacementProvider extends ModifierProvider {
 
     @Override
     public Mutator<?> defaultSetting() {
-        return drop;
+        return original;
     }
 
     @Override
